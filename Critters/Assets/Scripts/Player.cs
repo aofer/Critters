5c550cbcbbcee990e4505d5ab048c65063c26f4f
shmulzi <samya2005@gmail.com>
2014-05-30 01:32:27 +0300

    There is now a blood splatter animation when a character dies

--- Critters/Assets/Scripts/Player.cs
+++ Critters/Assets/Scripts/Player.cs
@@ -8,9 +8,11 @@ public class Player : MonoBehaviour {
 	private int playerNumber = 0;
 	private string playerCharacter = "none";
 	private bool outOfGame = false;
+	private worldAnimImpl worldScript;
 
-	void OnAwake () {	
-	
+	void Start () {	
+		GameObject worldAnim = GameObject.FindGameObjectWithTag("World Anim");
+		worldScript = worldAnim.GetComponent<worldAnimImpl>();
 	}
 	
 	// Update is called once per frame
@@ -51,10 +53,15 @@ public class Player : MonoBehaviour {
 		lives -= 1;
 	}
 	
+	public bool isOutOfGame(){
+		return outOfGame;
+	}
+	
 	public void die(){
 		loseLife();
 		GetComponent<character>().setIsDead(true);
 		GameObject playerPool = GameObject.FindGameObjectWithTag("Pool " + playerNumber);
+		worldScript.setAnimation("blood_splatter", transform.position);
 		transform.position = playerPool.transform.position;
 		if(!outOfGame){
 			Invoke("respawn",5);
@@ -68,10 +75,9 @@ public class Player : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
-		if(other.gameObject.tag.Equals("Player")){
-			print ("hit");
-			Player othPlayerScript = other.GetComponent<Player>();
-			othPlayerScript.die();
-		}
+//		if(other.gameObject.tag.Equals("Player")){
+//			Player othPlayerScript = other.GetComponent<Player>();
+//			othPlayerScript.die();
+//		}
 	}
 }
---
+++ Critters/Assets/Scripts/worldAnimImpl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class worldAnimImpl : MonoBehaviour {
+
+	private Animator bldAnim;
+	private Transform bldsplt;
+	
+	void Start () {
+		bldsplt = transform.GetChild(0);
+		bldAnim = bldsplt.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	public void setAnimation(string name, Vector2 location){
+		switch(name){
+			case "blood_splatter":
+				bldsplt.transform.position = location;
+				bldAnim.SetTrigger("Splat");
+				break;
+		}
+	}
+}
