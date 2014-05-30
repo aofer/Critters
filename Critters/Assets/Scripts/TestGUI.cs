da07140525af9ba9000e302aad8b7ed6ad5a1b5b
shmulzi <samya2005@gmail.com>
2014-05-31 02:40:23 +0300

    GUI Looks awesome now

--- Critters/Assets/Scripts/TestGUI.cs
+++ Critters/Assets/Scripts/TestGUI.cs
@@ -10,6 +10,9 @@ public class TestGUI : MonoBehaviour {
 	private Dictionary<int,Player> playerScripts = new Dictionary<int,Player>();
 	GameObject[] guiTexts;
 	
+	public GUISkin osdPlayerNames;
+	public GUISkin osdPlayerLives;
+	
 	void Start(){
 //		character = GameObject.FindGameObjectWithTag("Player");
 //		charScript = character.GetComponent<character>();
@@ -23,6 +26,8 @@ public class TestGUI : MonoBehaviour {
 				playerScripts.Add(playerScript.getPlayerNumber(),playerScript);
 			}
 		}
+
+		
 	}
 	void OnGUI(){
 //		GUI.Label(new Rect(Screen.width-200,0,150,150),"Velocity Y = " + charScript.getVelocityY());
@@ -31,7 +36,12 @@ public class TestGUI : MonoBehaviour {
 		Player player2script;
 		playerScripts.TryGetValue(1, out player1script);
 		playerScripts.TryGetValue(2, out player2script);
-		guiTexts[0].guiText.text = player1script.getLives().ToString();
-		guiTexts[1].guiText.text = player2script.getLives().ToString();
+
+		GUI.skin = osdPlayerNames;
+		GUI.Box(new Rect(Screen.width/12, Screen.height/12, 200, 100), "Bunny");
+		GUI.Box(new Rect(Screen.width/2 + 350, Screen.height/12, 200, 100), "Raccoon");
+		GUI.skin = osdPlayerLives;
+		GUI.Box(new Rect(Screen.width/12 + 200, Screen.height/12 - 10, 100, 100), player1script.getLives().ToString());
+		GUI.Box(new Rect(Screen.width/2 + 570, Screen.height/12 - 10, 100, 100), player2script.getLives().ToString());
 	}
 }
